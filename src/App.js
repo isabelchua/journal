c8ffd92e3c86e0c9728d27ec987e802540a42b5b
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-30 11:53:31 -0700

    added, reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-
+import React, { useContext, useReducer } from 'react';
 import './App.css';
+import NotesContext from './context';
+import notesReducer from './reducer';
 
 function App() {
-	return <div className="App">hello</div>;
+	const initialState = useContext(NotesContext);
+	const [state, dispatch] = useReducer(notesReducer, initialState);
+	return (
+		<NotesContext.Provider
+			value={{ state, dispatch }}
+		></NotesContext.Provider>
+	);
 }
 
 export default App;
