95f480c65f8d208c48b6067d3e4d9d144dfcf4cc
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-30 11:59:23 -0700

    css, nav

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useContext, useReducer } from 'react';
 import './App.css';
 import NotesContext from './context';
 import notesReducer from './reducer';
+import Nav from './components/Nav';
+//import Nav from './com'
 
 function App() {
 	const initialState = useContext(NotesContext);
 	const [state, dispatch] = useReducer(notesReducer, initialState);
 	return (
-		<NotesContext.Provider
-			value={{ state, dispatch }}
-		></NotesContext.Provider>
+		<NotesContext.Provider value={{ state, dispatch }}>
+			<Nav />
+		</NotesContext.Provider>
 	);
 }
 
