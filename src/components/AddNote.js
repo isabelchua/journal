efad701ba62e402e394640ee243b1632cba34f3d
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-30 13:31:32 -0700

    added ADD function

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
+import NotesContext from '../context';
 
 function AddNote() {
+	const { dispatch } = useContext(NotesContext);
+	const [value, setValue] = useState('');
+
+	let ref = useRef();
+
+	//focus on task bar
+	useEffect(() => {
+		ref.current.focus();
+	}, []);
+
+	const handleChange = e => {
+		setValue(e.target.value);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		//remove white spaces
+		if (value.trim() === '') {
+			alert('Add note!');
+		} else {
+			dispatch({ type: 'ADD_NOTE', payload: value });
+			setValue('');
+		}
+	};
+
 	return (
-		<div>
-			<form action="">
-				<input type="text" />
+		<div className="note-form">
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					ref={ref}
+					onChange={handleChange}
+					value={value}
+				/>
 				<button>add</button>
 			</form>
 		</div>
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,17 @@
+import nanoid from 'nanoid';
+
 export default function reducer(state, action) {
 	switch (action.type) {
+		case 'ADD_NOTE':
+			const newNote = {
+				id: nanoid(),
+				text: action.payload
+			};
+			const addedNotes = [...state.notes, newNote];
+			return {
+				...state,
+				notes: addedNotes
+			};
 		case 'SET_CURRENT_NOTE':
 			return {
 				...state,
